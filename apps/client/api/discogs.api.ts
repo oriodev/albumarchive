21f330aefe9eb254754fd8bb6825673a335550ba
oriodev <oriodev@duck.com>
2024-11-08 17:11:36 +0000

    fix(client): adds try/catch block to getAlbumsFromDiscogs

--- apps/client/api/discogs.api.ts
+++ apps/client/api/discogs.api.ts
@@ -2,13 +2,17 @@ export const getAlbumsFromDiscogs = async (search: string = "") => {
   const url = new URL(`${process.env.BACKEND_API}/discogs`);
   url.searchParams.append("search", search);
 
-  const response = await fetch(url);
+  try {
+    const response = await fetch(url);
 
-  if (!response.ok) {
-    throw new Error("Failed to fetch albums");
+    if (!response.ok) {
+      throw new Error("Failed to fetch albums");
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
   }
-
-  const data = await response.json();
-
-  return data;
 };
