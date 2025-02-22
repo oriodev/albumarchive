6d96fddd63a02027233786580aec708053ccea53
oriodev <oriodev@duck.com>
2025-02-22 15:28:11 +0000

    fix(client): album breadcrumbs display 'album' rather than the full id

--- apps/client/components/nav/breadcrumb-with-path.tsx
+++ apps/client/components/nav/breadcrumb-with-path.tsx
@@ -14,6 +14,8 @@ export function BreadcrumbWithPath() {
   const pathname = usePathname();
   const crumbs = pathname.substring(1).split("/");
 
+  // if crumbs[1] === 'albums' and we have crumbs[2] then crumbs[2] should equal 'album'
+
   const buildPath = (crumbs: string[], index: number) => {
     return crumbs.slice(0, index + 1).join("/");
   };
@@ -22,16 +24,23 @@ export function BreadcrumbWithPath() {
     <div>
       <Breadcrumb>
         <BreadcrumbList>
-          {crumbs.map((crumb, index) => (
-            <Fragment key={crumb}>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href={`/${buildPath(crumbs, index)}`}>
-                  <p>{crumb}</p>
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              {index < crumbs.length - 1 && <BreadcrumbSeparator />}
-            </Fragment>
-          ))}
+          {crumbs.map((crumb, index) => {
+            const crumbDisplay =
+              crumbs[1] === "albums" && crumbs[2] && index == 2
+                ? "album"
+                : crumb;
+
+            return (
+              <Fragment key={crumb}>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href={`/${buildPath(crumbs, index)}`}>
+                    <p>{crumbDisplay}</p>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                {index < crumbs.length - 1 && <BreadcrumbSeparator />}
+              </Fragment>
+            );
+          })}
         </BreadcrumbList>
       </Breadcrumb>
     </div>
