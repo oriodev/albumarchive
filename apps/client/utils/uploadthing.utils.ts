22d4f5d045fdf9b6207c481e4635c0c371b2b9d8
oriodev <oriodev@duck.com>
2025-03-15 17:56:33 +0000

    removed uploadthing utils

--- apps/client/utils/uploadthing.utils.ts
+++
@@ -1,8 +0,0 @@
-import { OurFileRouter } from "@/api/uploadthing/core";
-import {
-  generateUploadButton,
-  generateUploadDropzone,
-} from "@uploadthing/react";
-
-export const UploadButton = generateUploadButton<OurFileRouter>();
-export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
